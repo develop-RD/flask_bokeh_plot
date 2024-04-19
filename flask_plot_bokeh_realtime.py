1f67edf8008d882ecd82d2be9e044a1c611d18c2
megaloldean <megaloldean@gmail.com>
2024-04-19 19:20:21 +0300

    Настроил работу обновления ajax

---
+++ flask_plot_bokeh_realtime.py
@@ -0,0 +1,55 @@
+# Importing required functions 
+from flask import Flask, render_template, jsonify
+import numpy as np
+import random
+import time
+from threading import Thread
+# Flask constructor 
+app = Flask(__name__)
+
+# Формирую отсчёты
+time_x = np.linspace(0, 10, 500)
+signal_x = np.sin(time_x)
+
+#преобразовываю к типу list.нужно для отрисовки
+time_x = time_x.tolist()
+signal_x = signal_x.tolist()
+# Счётчик для инкремента
+count = 0
+
+sir = 65
+# функция обновления счётчика
+def update_count():
+    global count, signal_x, time_x
+    while True:
+        time_x = np.linspace(0, count, 500)
+        signal_x = np.sin(time_x)
+
+        #преобразовываю к типу list.нужно для отрисовки
+        time_x = time_x.tolist()
+        signal_x = signal_x.tolist()
+
+        count += 1
+        time.sleep(3)
+
+@app.route('/get_count', methods=['GET'])
+def get_count():
+    return jsonify({'count_n': count, 'sir':sir, 'data':signal_x,'labels':time_x})
+
+# формирую синус 
+@app.route('/')
+def homepage():
+    return render_template(template_name_or_list='chartjs-example.html',
+                           data=signal_x,
+                           labels=time_x)
+def runApp():
+    #app.run(host="localhost", port=8086, debug=False)
+    app.run(host="127.0.0.1", port=8086, debug=False)
+
+# Main Driver Function 
+if __name__ == '__main__':
+    import threading
+    t = threading.Thread(target=update_count)
+    t.start()
+    app.run(debug=True)
+
