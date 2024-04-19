eb9c7ff6336d652f4cb0e8146de1814b2099a272
megaloldean <megaloldean@gmail.com>
2024-04-19 08:14:16 +0300

    Восстановил работу графика

--- flask_plot_bokeh.py
+++ flask_plot_bokeh.py
@@ -15,42 +15,17 @@ signal_x = np.sin(time_x)
 time_x = time_x.tolist()
 signal_x = signal_x.tolist()
 
-# Генерация рандомного сигнала
-def randomSignal():
-    global signal_x
-    while(1):
-        time.sleep(2)
-        time_x = np.linspace(0, 10, 500)
-        signal_x = np.sin(time_x)*random.randrange(1,7)
-        print(signal_x)
-
-
 # формирую синус 
 @app.route('/')
 def homepage():
-    time.sleep(2)
-    time_x = np.linspace(0, 10, 500)
-    signal_x = np.sin(time_x)*random.randrange(1,7)
-    time_x = time_x.tolist()
-    signal_x = signal_x.tolist()
-
-    print(signal_x)
     return render_template(template_name_or_list='chartjs-example.html',
                            data=signal_x,
                            labels=time_x)
-#	return render_template(
-#		template_name_or_list='chartjs-example.html',
-#		data=signal_x,
-#		labels=time_x,
-#	)
 def runApp():
     #app.run(host="localhost", port=8086, debug=False)
     app.run(host="127.0.0.1", port=8086, debug=False)
 
 # Main Driver Function 
 if __name__ == '__main__':
-	# Run the application on the local development server ##
-#    t1 = Thread(target=randomSignal).start()
-    t2 = Thread(target=runApp).start()
-#	app.run(debug=True)
+	app.run(debug=True)
 
