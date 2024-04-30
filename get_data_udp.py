a940673bc1d6257f5781ea5d8c07d0cd6a5198cc
develop-RD <runov.denis93@gmail.com>
2024-04-30 19:28:45 +0300

    Добавил парсинга в обработчик пакетов по udp

--- get_data_udp.py
+++ get_data_udp.py
@@ -1,5 +1,7 @@
 import socket
 from struct import *
+
+import numpy as np
 UDP_IP = "127.0.0.1"
 UDP_PORT = 12345
 
@@ -8,6 +10,15 @@ sock.bind((UDP_IP, UDP_PORT))
 
 while True:
     data, addr = sock.recvfrom(5000)
-    print("Received data:", data)
+    #    print("Received data:", data)
     p_data = unpack("<if1024i",data);
-    print(p_data)
+    a = p_data[0]
+    b = p_data[1]
+    sin_m = p_data[2:]
+    print("\n sizeof = ",data.__sizeof__())
+    size_mass = len(sin_m)
+    print("\n size mass = ",size_mass)
+    count = np.linspace(0,1,size_mass)
+    print("\n count =",count)
+    print(f"a: {a}  b:{b},mass={sin_m}")
+
